d76eb1aff3cb3e95b134570ee9ccaa67eee150b8
mswym <masa.sawayama@gmail.com>
2022-05-13 13:45:24 +0200

    Update file

--- translucency/batch_run.py
+++ translucency/batch_run.py
@@ -2,18 +2,18 @@
 
 
 if __name__ == '__main__':
-    latent_dims = [16, 256, 2]
+    #latent_dims = [16, 256, 2]
 
-    from ae import *
-    batch_run_ae(latent_dims)
+    #from ae import *
+    #batch_run_ae(latent_dims)
 
-    from gan_dcgan import *
-    batch_run_dcgan(latent_dims)
+    #from gan_dcgan import *
+    #batch_run_dcgan(latent_dims)
 
     latent_dims = [4, 8, 32, 64, 128]
 
     from ae import *
     batch_run_ae(latent_dims)
 
-    from gan_dcgan import *
-    batch_run_dcgan(latent_dims)
+    #from gan_dcgan import *
+    #batch_run_dcgan(latent_dims)
