a703747bd8c4064bcffc592e9b27800f6ba0c0ad
mswym <masa.sawayama@gmail.com>
2022-05-13 18:22:45 +0200

    Update files

--- run_latent_decoder.py
+++ run_latent_decoder.py
@@ -59,6 +59,75 @@ def cal_linear_reg(x, new_coef_matrix, intercept):
 
     return x_hat.T + intercept
 
+
+def batch_run_ae(num_latent):
+    batch_size_train = 30
+    batch_size_test = 300
+    size_input = np.array([256, 256, 3])
+    scale_eigenval = 1
+    flag_autoadjust_eigenval = False
+
+    list_objname = ['armadillo', 'buddha', 'bun', 'bunny', 'bust', 'cap', 'cube', 'dragon', 'lucy', 'star_smooth',
+                    'sphere']
+    #list_objname = ['sphere', 'armadillo']
+    #list_num_latent = [2, 4, 8, 16, 32, 64, 128, 256]
+    #list_num_latent = [256]
+
+    path_dir_save = '/home/mswym/workspace/'
+    model_body = VAE_vanilla()
+
+    comb_list = list(itertools.combinations(np.linspace(0, len(list_objname) - 1, len(list_objname)), 2))
+
+    for ind_obj in range(len(comb_list)):
+        start_time = time.perf_counter()
+        ind_obj_1 = int(comb_list[ind_obj][0])
+        ind_obj_2 = int(comb_list[ind_obj][1])
+
+        path_save_1 = path_dir_save + "obj_mask/coef/coef_" + list_objname[ind_obj_1] + "-" + list_objname[ind_obj_2] + "_latent" + str(num_latent) + ".pkl"
+        path_save_2 = path_dir_save + "obj_mask/coef/coef_" + list_objname[ind_obj_2] + "-" + list_objname[ind_obj_1] + "_latent" + str(num_latent) + ".pkl"
+
+        path_checkpoint_1 = path_dir_save + "obj_mask/" + list_objname[
+            ind_obj_1] + "_latent" + str(num_latent) + "_logs/version_0/checkpoints/epoch=99-step=8499.ckpt"
+        path_checkpoint_2 = path_dir_save + "obj_mask/" + list_objname[
+            ind_obj_2] + "_latent" + str(num_latent) + "_logs/version_0/checkpoints/epoch=99-step=8499.ckpt"
+
+        mypath_1 = path_dir_save + 'model_objects_tonemap_mask/che_220322_1500train_' + list_objname[
+            ind_obj_1] + '.binary'
+        mypath_2 = path_dir_save + 'model_objects_tonemap_mask/che_220322_1500train_' + list_objname[
+            ind_obj_2] + '.binary'
+
+        img_transform = transforms.Compose([
+            transforms.Resize((size_input[0], size_input[1])),
+            transforms.ToTensor()
+        ])
+
+        img_train_1 = read_img_dataset(mypath_1, img_transform, batch_size_train)
+        img_train_2 = read_img_dataset(mypath_2, img_transform, batch_size_train)
+
+        #latent computing.
+        latent_z_1 = extract_latents(len(img_train_1), model_body, path_checkpoint_1, img_train_1)
+        latent_z_2 = extract_latents(len(img_train_2), model_body, path_checkpoint_2, img_train_2)
+
+        # train the linear decoder
+        decoder_12 = make_decoder(latent_z_1, latent_z_2, scale_eigenval,
+                                                        autoadjust_eigenval=flag_autoadjust_eigenval)
+        decoder_21 = make_decoder(latent_z_2, latent_z_1, scale_eigenval,
+                                                        autoadjust_eigenval=flag_autoadjust_eigenval)
+
+        with open(path_save_1, 'wb') as f:
+            pickle.dump(decoder_12, f)
+        with open(path_save_2, 'wb') as f:
+            pickle.dump(decoder_21, f)
+        print(time.perf_counter()-start_time)
+
+        #latent_z_12 = cal_linear_reg(latent_z_1, decoder_12['coefficient'], decoder_12['intercept'])
+        #latent_z_21 = cal_linear_reg(latent_z_2, decoder_21['coefficient'], decoder_21['intercept'])
+        #plt.scatter(latent_z_21.flatten(),latent_z_1.flatten())
+        #plt.show()
+        #plt.scatter(latent_z_12.flatten(), latent_z_2.flatten())
+        #plt.show()
+
+
 if __name__ == '__main__':
     batch_size_train = 30
     batch_size_test = 300
